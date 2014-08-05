e6b4aaae7b6643da4d04ce528d1cdc0bfbe82e77
Aditya Palepu <apalepu23@gmail.com>
2014-08-05 01:48:57 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yumster::Application.config.secret_key_base = '5d1c6a57d2da4cf73e42b4ae45cf5f2071e7cd9ac05d1c45a45fb88fbdba9e3205e1b5d10ca84b8469b3579cca3cbd49d493548ae5b472b0c4f21773aa2e2ae5'
